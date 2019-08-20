f8a7655626c3365cfd640da4862db0e17ec28943
Tyler Glaiel <TylerGlaiel@users.noreply.github.com>
2019-08-20 13:16:54 -0700

    Style Cleanup Customizable Error Handling Combining Functions (Append, ShallowMerge, DeepMerge) Compatibility with range-based for loops Indentation on output

--- gon.cpp
+++ gon.cpp
@@ -16,6 +16,23 @@ bool IsIgnoredSymbol(char c){
     return c=='='||c==','||c==':';
 }
 
+void ThrowString(std::string err){
+    throw err;
+}
+
+std::function<void(std::string)> GonObject::OnError = ThrowString;
+const GonObject GonObject::null_gon;
+
+GonObject::MergeMode GonObject::MergePolicyAppend(const GonObject& field_a, const GonObject& field_b){
+    return MergeMode::APPEND;
+}
+GonObject::MergeMode GonObject::MergePolicyDeepMerge(const GonObject& field_a, const GonObject& field_b){
+    return MergeMode::DEEPMERGE;
+}
+GonObject::MergeMode GonObject::MergePolicyOverwrite(const GonObject& field_a, const GonObject& field_b){
+    return MergeMode::OVERWRITE;
+}
+
 std::vector<std::string> Tokenize(std::string data){
     std::vector<std::string> tokens;
 
@@ -70,7 +87,6 @@ std::vector<std::string> Tokenize(std::string data){
             current_token += data[i];
         }
 
-        //TODO: escape sequences
         if(inString){
             if(escaped){
                 if(data[i] == 'n'){
@@ -132,7 +148,7 @@ struct TokenStream {
 
 GonObject::GonObject(){
     name = "";
-    type = g_null;
+    type = FieldType::NULLGON;
     int_data = 0;
     float_data = 0;
     bool_data = 0;
@@ -143,7 +159,7 @@ GonObject LoadFromTokens(TokenStream& Tokens){
     GonObject ret;
 
     if(Tokens.Peek() == "{"){         //read object
-        ret.type = GonObject::g_object;
+        ret.type = GonObject::FieldType::OBJECT;
 
         Tokens.Read(); //consume '{'
 
@@ -154,48 +170,54 @@ GonObject LoadFromTokens(TokenStream& Tokens){
             ret.children_map[name] = ret.children_array.size()-1;
             ret.children_array[ret.children_array.size()-1].name = name;
 
-            if(Tokens.error) throw "GON ERROR: missing a '}' somewhere";
+            if(Tokens.error) {
+                GonObject::OnError("GON ERROR: missing a '}' somewhere");
+                return GonObject::null_gon;
+            }
         }
 
         Tokens.Read(); //consume '}'
 
         return ret;
     } else if(Tokens.Peek() == "["){  //read array
-        ret.type = GonObject::g_array;
+        ret.type = GonObject::FieldType::ARRAY;
 
         Tokens.Read(); //consume '['
         while(Tokens.Peek() != "]"){
             ret.children_array.push_back(LoadFromTokens(Tokens));
 
-            if(Tokens.error) throw "GON ERROR: missing a ']' somewhere";
+            if(Tokens.error) {
+                GonObject::OnError("GON ERROR: missing a ']' somewhere");
+                return GonObject::null_gon;
+            }
         }
         Tokens.Read(); //consume ']'
 
         return ret;
     } else {                          //read data value
-        ret.type = GonObject::g_string;
+        ret.type = GonObject::FieldType::STRING;
         ret.string_data = Tokens.Read();
 
         //if string data can be converted to a number, do so
         char*endptr;
         ret.int_data = strtol(ret.string_data.c_str(), &endptr, 0);
         if(*endptr == 0){
-            ret.type = GonObject::g_number;
+            ret.type = GonObject::FieldType::NUMBER;
         }
 
         ret.float_data = strtod(ret.string_data.c_str(), &endptr);
         if(*endptr == 0){
-            ret.type = GonObject::g_number;
+            ret.type = GonObject::FieldType::NUMBER;
         }
 
         //if string data can be converted to a bool or null, convert
-        if(ret.string_data == "null") ret.type = GonObject::g_null;
+        if(ret.string_data == "null") ret.type = GonObject::FieldType::NULLGON;
         if(ret.string_data == "true") {
-            ret.type = GonObject::g_bool;
+            ret.type = GonObject::FieldType::BOOL;
             ret.bool_data = true;
         }
         if(ret.string_data == "false") {
-            ret.type = GonObject::g_bool;
+            ret.type = GonObject::FieldType::BOOL;
             ret.bool_data = false;
         }
 
@@ -239,42 +261,42 @@ GonObject GonObject::LoadFromBuffer(std::string buffer){
 
 //options with error throwing
 std::string GonObject::String() const {
-    if(type != g_string && type != g_number && type != g_bool) throw "GSON ERROR: Field is not a string";
+    if(type != FieldType::STRING && type != FieldType::NUMBER && type != FieldType::BOOL) OnError("GON ERROR: Field is not a string");
     return string_data;
 }
 int GonObject::Int() const {
-    if(type != g_number) throw "GON ERROR: Field is not a number";
+    if(type != FieldType::NUMBER) OnError("GON ERROR: Field is not a number");
     return int_data;
 }
 double GonObject::Number() const {
-    if(type != g_number) throw "GON ERROR: Field is not a number";
+    if(type != FieldType::NUMBER) OnError("GON ERROR: Field is not a number");
     return float_data;
 }
 bool GonObject::Bool() const {
-    if(type != g_bool) throw "GON ERROR: Field is not a bool";
+    if(type != FieldType::BOOL) OnError("GON ERROR: Field is not a bool");
     return bool_data;
 }
 
 //options with a default value
 std::string GonObject::String(std::string _default) const {
-    if(type != g_string && type != g_number && type != g_bool) return _default;
+    if(type != FieldType::STRING && type != FieldType::NUMBER && type != FieldType::BOOL) return _default;
     return string_data;
 }
 int GonObject::Int(int _default) const {
-    if(type != g_number) return _default;
+    if(type != FieldType::NUMBER) return _default;
     return int_data;
 }
 double GonObject::Number(double _default) const {
-    if(type != g_number) return _default;
+    if(type != FieldType::NUMBER) return _default;
     return float_data;
 }
 bool GonObject::Bool(bool _default) const {
-    if(type != g_bool) return _default;
+    if(type != FieldType::BOOL) return _default;
     return bool_data;
 }
 
 bool GonObject::Contains(std::string child) const{
-    if(type != g_object) return false;
+    if(type != FieldType::OBJECT) return false;
 
     std::map<std::string, int>::const_iterator iter = children_map.find(child);
     if(iter != children_map.end()){
@@ -284,19 +306,24 @@ bool GonObject::Contains(std::string child) const{
     return false;
 }
 bool GonObject::Contains(int child) const{
-    if(type != g_object && type != g_array) return true;
+    if(type != FieldType::OBJECT && type != FieldType::ARRAY) return true;
 
     if(child < 0) return false;
     if(child >= children_array.size()) return false;
     return true;
 }
 bool GonObject::Exists() const{
-    return type != g_null;
+    return type != FieldType::NULLGON;
+}
+
+const GonObject& GonObject::ChildOrSelf(std::string child) const{
+    if(Contains(child)) return (*this)[child];
+    return *this;
 }
 
 const GonObject& GonObject::operator[](std::string child) const {
-    if(type == g_null) return null_gon;
-    if(type != g_object) throw "GON ERROR: Field is not an object";
+    if(type == FieldType::NULLGON) return null_gon;
+    if(type != FieldType::OBJECT) return null_gon;
 
     std::map<std::string, int>::const_iterator iter = children_map.find(child);
     if(iter != children_map.end()){
@@ -304,21 +331,39 @@ const GonObject& GonObject::operator[](std::string child) const {
     }
 
     return null_gon;
-    //throw "GON ERROR: Field not found: "+child;
 }
 const GonObject& GonObject::operator[](int childindex) const {
-    if(type != g_object && type != g_array) return *this;//throw "GSON ERROR: Field is not an object or array";
+    if(type != FieldType::OBJECT && type != FieldType::ARRAY) return *this;
     return children_array[childindex];
 }
-int GonObject::Length() const {
-    if(type != g_object && type != g_array) return 1;//size 1, object is self    //throw "GSON ERROR: Field is not an object or array";
-    return children_array.size();
+int GonObject::Size() const {
+    return size();
 }
 
 
+int GonObject::size() const {
+    if(type != FieldType::OBJECT && type != FieldType::ARRAY) return 1;//size 1, object is self
+    return children_array.size();
+}
+const GonObject* GonObject::begin() const {
+    if(type != FieldType::OBJECT && type != FieldType::ARRAY) return this;
+    return children_array.data();
+}
+const GonObject* GonObject::end() const {
+    if(type != FieldType::OBJECT && type != FieldType::ARRAY) return this+1;
+    return children_array.data()+children_array.size();
+}
+GonObject* GonObject::begin() {
+    if(type != FieldType::OBJECT && type != FieldType::ARRAY) return this;
+    return children_array.data();
+}
+GonObject* GonObject::end() {
+    if(type != FieldType::OBJECT && type != FieldType::ARRAY) return this+1;
+    return children_array.data()+children_array.size();
+}
 
 void GonObject::DebugOut(){
-    if(type == g_object){
+    if(type == FieldType::OBJECT){
         std::cout << name << " is object {" << std::endl;
         for(int i = 0; i<children_array.size(); i++){
             children_array[i].DebugOut();
@@ -326,7 +371,7 @@ void GonObject::DebugOut(){
         std::cout << "}" << std::endl;
     }
 
-    if(type == g_array){
+    if(type == FieldType::ARRAY){
         std::cout << name << " is array [" << std::endl;
         for(int i = 0; i<children_array.size(); i++){
             children_array[i].DebugOut();
@@ -334,19 +379,19 @@ void GonObject::DebugOut(){
         std::cout << "]" << std::endl;
     }
 
-    if(type == g_string){
+    if(type == FieldType::STRING){
         std::cout << name << " is string \"" << String() << "\"" << std::endl;
     }
 
-    if(type == g_number){
+    if(type == FieldType::NUMBER){
         std::cout << name << " is number " << Int() << std::endl;
     }
 
-    if(type == g_bool){
+    if(type == FieldType::BOOL){
         std::cout << name << " is bool " << Bool()<< std::endl;
     }
 
-    if(type == g_null){
+    if(type == FieldType::NULLGON){
         std::cout << name << " is null " << std::endl;
     }
 }
@@ -355,39 +400,64 @@ void GonObject::DebugOut(){
 void GonObject::Save(std::string filename){
     std::ofstream outfile(filename);
     for(int i = 0; i<children_array.size(); i++){
-        outfile << children_array[i].name+" "+children_array[i].getOutStr()+"\n";
+        outfile << children_array[i].name+" "+children_array[i].GetOutStr()+"\n";
     }
     outfile.close();
 }
 
-std::string GonObject::getOutStr(){
+std::string GonObject::GetOutStr(std::string tab, std::string current_tab){
     std::string out = "";
 
-    if(type == g_object){
+    if(type == FieldType::OBJECT){
         out += "{\n";
         for(int i = 0; i<children_array.size(); i++){
-            out += children_array[i].name+" "+children_array[i].getOutStr()+"\n";
+            out += current_tab+tab+children_array[i].name+" "+children_array[i].GetOutStr(tab, tab+current_tab);
+            if(out.back() != '\n') out += "\n";
         }
-        out += "}\n";
+        out += current_tab+"}\n";
     }
 
-    if(type == g_array){
-        out += "[";
+    if(type == FieldType::ARRAY){
+        bool short_array = true;
         for(int i = 0; i<children_array.size(); i++){
-            out += children_array[i].getOutStr()+" ";
+            if(children_array[i].type == GonObject::FieldType::ARRAY)
+                short_array = false;
+
+            if(children_array[i].type == GonObject::FieldType::OBJECT)
+                short_array = false;
+
+            if(children_array[i].type == GonObject::FieldType::STRING && children_array[i].String().size() > 10)
+                short_array = false;
+
+            if(!short_array) break;
         }
-        out += "]\n";
+
+        if(short_array){
+            out += "[";
+            for(int i = 0; i<children_array.size(); i++){
+                out += children_array[i].GetOutStr(tab, tab+current_tab);
+                if(i != children_array.size()-1) out += " ";
+            }
+            out += "]\n";
+        } else {
+            out += "[\n";
+            for(int i = 0; i<children_array.size(); i++){
+                out += current_tab+tab+children_array[i].GetOutStr(tab, tab+current_tab);
+                if(out.back() != '\n') out += "\n";
+            }
+            out += current_tab+"]\n";
+        }
     }
 
-    if(type == g_string){
+    if(type == FieldType::STRING){
         out += String();
     }
 
-    if(type == g_number){
+    if(type == FieldType::NUMBER){
         out += std::to_string(Int());
     }
 
-    if(type == g_bool){
+    if(type == FieldType::BOOL){
         if(Bool()){
             out += "true";
         } else {
@@ -395,11 +465,94 @@ std::string GonObject::getOutStr(){
         }
     }
 
-    if(type == g_null){
+    if(type == FieldType::NULLGON){
         out += "null";
     }
 
     return out;
 }
 
-GonObject GonObject::null_gon;
+
+void GonObject::Append(const GonObject& other){
+    if(type == FieldType::NULLGON){
+        *this = other;
+    } if(type == FieldType::OBJECT && other.type == FieldType::OBJECT){
+        for(int i = 0; i<other.size(); i++){
+            children_array.push_back(other[i]);
+            children_map[other[i].name] = children_array.size() - 1;
+        }
+    } else if(type == FieldType::ARRAY && other.type == FieldType::ARRAY){
+        for(int i = 0; i<other.size(); i++){
+            children_array.push_back(other[i]);
+        }
+    } else {
+        OnError("GON ERROR: Cannot Shallow Merge incompatible types");
+    }
+}
+
+void GonObject::ShallowMerge(const GonObject& other, std::function<void(const GonObject& a, const GonObject& b)> OnOverwrite){
+    if(type == FieldType::NULLGON){
+        *this = other;
+    } else if(type == FieldType::OBJECT && other.type == FieldType::OBJECT){
+        for(int i = 0; i<other.size(); i++){
+            if(Contains(other[i].name)){
+                if(OnOverwrite) OnOverwrite((*this)[other[i].name], other[i]);
+                children_array[children_map[other[i].name]] = other[i];
+            } else {
+                children_array.push_back(other[i]);
+                children_map[other[i].name] = children_array.size() - 1;
+            }
+        }
+    } else if(type == FieldType::ARRAY && other.type == FieldType::ARRAY){
+        for(int i = 0; i<other.size(); i++){
+            children_array.push_back(other[i]);
+        }
+    } else {
+        OnError("GON ERROR: Cannot Shallow Merge incompatible types");
+    }
+}
+
+
+void GonObject::DeepMerge(const GonObject& other, MergePolicyCallback ObjectMergePolicy, MergePolicyCallback ArrayMergePolicy){
+    if(type == FieldType::OBJECT && other.type == FieldType::OBJECT){
+        MergeMode policy = ObjectMergePolicy(*this, other);
+
+        if(policy == MergeMode::APPEND){
+            for(int i = 0; i<other.size(); i++){
+                children_array.push_back(other[i]);
+                children_map[other[i].name] = children_array.size() - 1;
+            }
+        } else if(policy == MergeMode::DEEPMERGE) {
+            for(int i = 0; i<other.size(); i++){
+                if(Contains(other[i].name)){
+                    children_array[children_map[other[i].name]].DeepMerge(other[i]);
+                } else {
+                    children_array.push_back(other[i]);
+                    children_map[other[i].name] = children_array.size() - 1;
+                }
+            }
+        } else if(policy == MergeMode::OVERWRITE) {
+            *this = other;
+        }
+    } else if(type == FieldType::ARRAY && other.type == FieldType::ARRAY){
+        MergeMode policy = ArrayMergePolicy(*this, other);
+
+        if(policy == MergeMode::APPEND){
+            for(int i = 0; i<other.size(); i++){
+                children_array.push_back(other[i]);
+            }
+        } else if(policy == MergeMode::DEEPMERGE) {
+            for(int i = 0; i<other.size(); i++){
+                if(i < size()){
+                    children_array[i].DeepMerge(other[i]);
+                } else {
+                    children_array.push_back(other[i]);
+                }
+            }
+        } else if(policy == MergeMode::OVERWRITE) {
+            *this = other;
+        }
+    } else {
+        *this = other;
+    }
+}
--- gon.h
+++ gon.h
@@ -5,18 +5,30 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <functional>
 
 class GonObject {
     public:
-        enum gon_type {
-            g_null,
+        static const GonObject null_gon;
+        //default just throws the string, can be set if you want to avoid exceptions
+        static std::function<void(std::string)> OnError;
 
-            g_string,
-            g_number,
-            g_object,
-            g_array,
-            g_bool
+        enum class FieldType {
+            NULLGON,
+
+            STRING,
+            NUMBER,
+            OBJECT,
+            ARRAY,
+            BOOL
         };
+
+        enum class MergeMode {
+            APPEND,
+            DEEPMERGE,
+            OVERWRITE
+        };
+
 
         std::map<std::string, int> children_map;
         std::vector<GonObject> children_array;
@@ -25,14 +37,15 @@ class GonObject {
         bool bool_data;
         std::string string_data;
 
-        static GonObject null_gon;
+        std::string name;
+        FieldType type;
 
-    public:
+        static MergeMode MergePolicyAppend(const GonObject& field_a, const GonObject& field_b);
+        static MergeMode MergePolicyDeepMerge(const GonObject& field_a, const GonObject& field_b);
+        static MergeMode MergePolicyOverwrite(const GonObject& field_a, const GonObject& field_b);
         static GonObject Load(std::string filename);
         static GonObject LoadFromBuffer(std::string buffer);
-
-        std::string name;
-        int type;
+        typedef std::function<MergeMode(const GonObject& field_a, const GonObject& field_b)> MergePolicyCallback;
 
         GonObject();
 
@@ -55,13 +68,47 @@ class GonObject {
         //returns null_gon if the field does not exist. 
         const GonObject& operator[](std::string child) const;
 
+        //returns self if child does not exist (useful for stuff that can either be a child or the default property of a thing)
+        const GonObject& ChildOrSelf(std::string child) const;
+
         //returns self if index is not an array, 
-        //all objects can be considered an array of size 1 with themselves as the member
+        //all objects can be considered an array of size 1 with themselves as the member, if they are not an ARRAY or an OBJECT
         const GonObject& operator[](int childindex) const;
-        int Length() const;
+        int Size() const;
 
+        //compatability with std
+        //all objects can be considered an array of size 1 with themselves as the member, if they are not an ARRAY or an OBJECT
+        int size() const;
+        const GonObject* begin() const;
+        const GonObject* end() const;
+        GonObject* begin();
+        GonObject* end();
+
+
+        //mostly used for debugging, as GON is not meant for saving files usually
         void DebugOut();
-
         void Save(std::string outfilename);
-        std::string getOutStr();
+        std::string GetOutStr(std::string tab = "    ", std::string current_tab = "");
+
+        //merging/combining functions
+        //if self and other are a gon_object: other will be appended to self
+        //if self and other are a gon_array: other will be appended to self
+        //if self is a null gon, overwrite self with other
+        //otherwise: error
+        //(note if a field with the same name is used multiple times, the most recently added one is mapped to the associative array lookup table, however duplicate fields will still exist)
+        void Append(const GonObject& other);
+
+        //if self and other are a gon_object: fields with matching names will be overwritten, new fields appended
+        //if self and other are a gon_array: other will be appended to self
+        //if self is a null gon, overwrite self with other
+        //otherwise: error
+        //(OnOverwrite can be specified if you want a warning or error if gons contain overlapping members)
+        void ShallowMerge(const GonObject& other, std::function<void(const GonObject& a, const GonObject& b)> OnOverwrite = NULL);
+                                                  
+                                                  
+        //if self and other are a gon_object: fields with matching names will be DeepMerged, new fields appended
+        //if self and other are a gon_array: fields with matching indexes will be DeepMerged, additional fields appended
+        //if self and other mismatch: other will overwrite self
+        //ObjectMergePolicy and ArrayMergePolicy can be specified if you want to change how fields merge on a per-field basis
+        void DeepMerge(const GonObject& other, MergePolicyCallback ObjectMergePolicy = MergePolicyDeepMerge, MergePolicyCallback ArrayMergePolicy = MergePolicyDeepMerge);
 };
